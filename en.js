fd006e25ee1f189c9f5d8443812d127602e1c9a9
renkun <kun.ren@coolcollege.cn>
2021-04-02 19:23:32 +0800

    feat: 读取js的翻译文件

---
+++ en.js
@@ -0,0 +1,7 @@
+export default {
+  common: {
+    noDataTextTip: 'No content',
+    listNoData:    'No more'
+  },
+  language: 'en'
+};
--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 #!/usr/bin/env node
 "use strict";
 const { Command } = require('commander');
-const log = require("./lib/logging")
+const path = require('path');
+const log = require("./lib/logging");
 const program = new Command();
-
 const { version } = require('./package.json');
+
 program
   .version(version, '-v,--version', 'output the current version')
-  .description('Machine learning translate any language by Baidu api.')
-  .command('init', 'init translate service')
-  .command('run', 'start service, or all if no name supplied');
+  .description('Machine learning translate any language.')
+  .arguments('<from> <to>')
+  .action((from, to) => {
+    // 从from文件中导出翻译对象
+    let str = ;
+    // log.info(str);
+    let fromObject = eval('('+str+')');
+    log.info(fromObject.common);
+    // 从to文件中导出翻译对象
+    // let toObject = require(path.resolve(basePath,to));
+    // for (const key in fromObject) {
+    //   if (Object.hasOwnProperty.call(fromObject, key)) {
+    //     const element = fromObject[key];
+    //     log.info(element)
+    //   }
+    // }
+    // log.info(fromObject)
+    // log.info(toObject)
+    // 比对 from 和 to 对象的差异，返回差异对象
+    // 翻译差异对象
+    // 写入插入对象
+
+  })
+  .option('-p, --platform [name]', 'designated translation platform', 'baidu');
 
 program.parse()
